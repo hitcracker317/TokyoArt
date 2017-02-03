e5277905121ea35f0ddf2c71d36a60c4d54f75ea
hitcracker317 <dainaso97@gmail.com>
2017-02-03 17:15:13 +0900

    通信してDOM生成

--- src/js/event.js
+++ src/js/event.js
@@ -1,30 +1,53 @@
 $(function(){
   var url = "http://www.tokyoartbeat.com/list/event_searchNear?Latitude=35.671208&Longitude=139.76517&Schedule=upcoming&SortOrder=mostpopular";
-  $.ajax({
-      type: "GET",
-      url: url,
-      dataType: 'xml',
-      success: function(data){
+  getEvent(url);
 
-        $(data.responseText).find("event").each(function(){
-
-           var eventText = $(this).children('name').text();
-           var placeText = $(this).find('venue name').text();
-           var accessText = $(this).find('datestart').text();
-           var startTime = $(this).find('dateend').text();
-           var eventImage = $(this).find('img')[2];
-           console.log(eventImage);
-
-           var event = "<div class='event'>" + eventText + placeText + accessText + accessText + " ~ " + startTime + "</div>";
-           var img = eventImage;
-
-           $(".main__inner").append(eventImage);
-           $(".main__inner").append(event);
-
-        });
-
-      }, error: function(e) {
-        console.log("エラー：" + JSON.stringify(e));
-      }
-  });
 });
+
+function getEvent(url) {
+  //API叩く
+  
+  $.ajax({
+    type: "GET",
+    url: url,
+    dataType: 'xml',
+    success: function(data){
+      var event = $(data.responseText).find("event");
+      event.each(function(){
+        makeEventDom($(this));
+      });
+
+    }, error: function(e) {
+      console.log("エラー：" + JSON.stringify(e));
+    }
+  });
+}
+
+function makeEventDom(event) {
+  var titleContent = event.children('name').text();
+  var placeContent = event.find('venue name').text();
+  var addressContent = event.find('address').text();
+  var startTimeContent = event.find('DateStart').text();
+  var endTimeContent = event.find('DateEnd').text();
+  var imageContent = event.find('img')[2];
+
+  var eventWrapper = $("<div class='event__wrapper'></div>");
+  var eventImage = $("<div class='event__image'><div class='event__image__inner'></div></div>");
+  var eventInfo = $("<div class='event__info'></div>");
+  var eventTitleText = "<div class='event__title event__text'><p>" + titleContent + "</p></div>";
+  var eventPlaceText = "<div class='event__place event__text'><p>" + placeContent + "</p></div>";
+  var eventAddressText = "<div class='event__address event__text'><p>" + addressContent + "</p></div>";
+  var eventDateText = "<div class='event__date event__text'><p>" + startTimeContent + "~" + endTimeContent + "</p></div>";
+
+  //イベント要素
+  $(".main__inner").append(eventWrapper);
+  //画像
+  eventWrapper.append(eventImage);
+  eventImage.find(".event__image__inner").append(imageContent);
+  //テキスト情報
+  eventWrapper.append(eventInfo);
+  eventInfo.append(eventTitleText);
+  eventInfo.append(eventPlaceText);
+  eventInfo.append(eventAddressText);
+  eventInfo.append(eventDateText);
+}
