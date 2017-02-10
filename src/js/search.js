7d7f9cff9ee821481a0f4e995178198f85c15e33
hitcracker317 <dainaso97@gmail.com>
2017-02-10 12:39:11 +0900

    検索モーダルを表示時にアニメーションを付与

--- src/js/search.js
+++ src/js/search.js
@@ -3,19 +3,44 @@ $(function(){
   var scrollPos;
 
   $(".js--search").click(function(){
-    //モーダルを表示
-    scrollPos = $(window).scrollTop();
-    $("body").addClass("body__fixed").css({top: -scrollPos});
-    $(".search--js").fadeIn(100).css({top: scrollPos});
+    searchOpen(); //モーダルを表示
   });
 
   $(".search__close--js").click(function(){
-    //モーダルを非表示
-
-
-    $("body").removeClass("body__fixed").css({top: 0});
-    $(window).scrollTop(scrollPos);
-    $(".search--js").fadeOut(100);
-    $(".search--js").css("display", "none");
+    searchClose(); //モーダルを非表示
   });
 });
+
+
+function searchOpen() {
+  scrollPos = $(window).scrollTop();
+  $("body").addClass("body__fixed").css({top: -scrollPos});
+  $(".search--js").fadeIn(300).css({top: scrollPos});
+
+  $(".search__container--js").velocity(
+    {
+      top: "50%"
+    }, {
+      duration: 300,
+      easing: 'ease-in-out'
+    }
+  );
+}
+
+function searchClose() {
+  $("body").removeClass("body__fixed").css({top: 0});
+  $(window).scrollTop(scrollPos);
+
+  $(".search__container--js").velocity(
+    {
+      top: "-25%"
+    }, {
+      duration: 300,
+      easing: 'ease-in-out',
+      complete: function(){
+        $(".search--js").fadeOut(100);
+        $(".search--js").css("display", "none");
+      }
+    }
+  );
+}
